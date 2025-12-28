b63f8cb7bb81c4ca5fc7ad55cec45489a4ce13cc
suvancim <albamariade2836@hotmail.com>
2025-12-28 18:54:44 -0300

    feat: Add relativistic correction to antihelium-4 ID

--- analysis/antihelium_id.py
+++ analysis/antihelium_id.py
@@ -1,8 +1,16 @@
+import math
+
+def relativistic_gamma(momentum, mass):
+    """Calculate relativistic gamma factor."""
+    energy = math.sqrt(momentum**2 + mass**2)
+    return energy / mass
+
 def calculate_mass_to_charge(momentum, charge):
-    """Calculate mass-to-charge ratio for antihelium-4."""
-    # simple non-relativistic approximation
-    mass = 3.727  # GeV/c^2
-    return mass / charge
+    """Calculate mass-to-charge ratio for antihelium-4 with relativistic correction."""
+    mass_rest = 3.727  # GeV/c^2
+    gamma = relativistic_gamma(momentum, mass_rest)
+    mass_rel = mass_rest * gamma
+    return mass_rel / charge
 
 def identify_antihelium4(momentum, charge, detector_signals):
     """Identify antihelium-4 nuclei based on mass-to-charge and detector signals."""
